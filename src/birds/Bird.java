a2e1b4a7adbd022377848294b755311a148f2e15
AstroARTY <artem.mirgorod@gmail.com>
2019-04-19 16:23:37 +0300

    init birds

---
+++ src/birds/Bird.java
@@ -0,0 +1,7 @@
+package birds;
+
+public class Bird {
+  // TODO fix class declaration
+
+  // TODO override toString method by returning a string with the class name
+}
---
+++ src/birds/FlyingBird.java
@@ -0,0 +1,9 @@
+package birds;
+
+public abstract class FlyingBird extends Bird {
+  // TODO fix class declaration
+
+  public void checkWings() {
+    // TODO print that bird wings are checked
+  }
+}
