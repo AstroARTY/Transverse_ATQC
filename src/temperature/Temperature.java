9d3ac19db3f63b1e4c5eb0d87d18ffed6db94972
Artem Myrgorod <amyrgor@softserveinc.com>
2019-04-26 10:57:55 +0300

    temperature converter

---
+++ src/temperature/Temperature.java
@@ -0,0 +1,28 @@
+package temperature;
+
+public class Temperature {
+
+  /**
+   * Converts temperature from Fahrenheit to Celsius degree.
+   *
+   * @param fahrenheit temperature degree in Fahrenheit
+   * @return temperature degree in Celsius
+   */
+  public static double fahrenheitToCelsius(double fahrenheit) {
+    double celcius;
+    celcius = 5 * (fahrenheit - 32) / 9;
+    return celcius;
+  }
+
+  /**
+   * Converts temperature from Celsius to Fahrenheit degree.
+   *
+   * @param celsius temperature degree in Celsius
+   * @return temperature degree in Fahrenheit
+   */
+  public static double celsiusToFahrenheit(double celsius) {
+    double fahrenheit;
+    fahrenheit = (9 * celsius + 160) / 5;
+    return fahrenheit;
+  }
+}
---
+++ src/temperature/TemperatureTest.java
@@ -0,0 +1,53 @@
+package temperature;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TemperatureTest {
+
+  /**
+   * Test data for Fahrenheit to Celsius conversion.
+   * @return test data array
+   */
+  @DataProvider(name = "Fahrenheit")
+  public static Object[][] fahrenheit() {
+    return new Object[][]{
+        {212.0, 100.0},
+        {32.0, 0.0},
+        {-15, -26.11111111111111},
+        {10000, 5537.777777777777},
+        {100, 37.77777777777778},
+    };
+  }
+
+  /**
+   * Test data for Celsius to Fahrenheit conversion.
+   * @return test data array
+   */
+  @DataProvider(name = "Celsius")
+  public static Object[][] celsius() {
+    return new Object[][]{
+        {100.0, 212.0},
+        {0.0, 32.0},
+        {-26.11111111111111, -15},
+        {0.11111, 32.199998},
+        {5504.85, 9940.73}
+    };
+  }
+
+  @Test(dataProvider = "Fahrenheit")
+  public void testFahrenheitToCelsius(double fahrenheit, double expectedResult) {
+    double actulResult = Temperature.fahrenheitToCelsius(fahrenheit);
+
+    assertEquals(actulResult, expectedResult, "Conversion from Fahrenheit to Celsius is wrong");
+  }
+
+  @Test(dataProvider = "Celsius")
+  public void testCelsiusToFahrenheit(double celsius, double expectedResult) {
+    double actulResult = Temperature.celsiusToFahrenheit(celsius);
+
+    assertEquals(actulResult, expectedResult, "Conversion from Celsius to Fahrenheit is wrong");
+  }
+}
